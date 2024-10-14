2f853bab61d0bdb9e357db7b923e657ff793a58f
Kewl <87680132+ac8736@users.noreply.github.com>
2024-10-14 15:47:50 -0400

    Removed some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import "./App.css";
 import Home from "./pages/Home";
-// import { useState } from "react";
-
-// interface Column {
-//   name: string;
-//   type: string;
-// }
 
 function App() {
   return (
